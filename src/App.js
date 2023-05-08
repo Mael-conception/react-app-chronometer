92101dcfb90b5469c1d60246189147b746d7b46f
vanotis720 <vanotis720@gmail.com>
2023-05-08 18:10:51 +0200

    instructions for exercices

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ function App() {
 	const [dateToday, setDateToday] = useState(new Date());
 	const [auth, setAuth] = useState(false);
 
+
+
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setDateToday(new Date());
 			setAuth(true);
+
+			// code here: fetch data from api
+			// put it in state
 		}, 1000);
 
 		return () => {
+			// clear the previous state
 			clearInterval(interval);
 			console.log('unmouted');
 		}
@@ -24,6 +30,7 @@ function App() {
 		<div className="App">
 			<h1 id="">Hello</h1>
 
+			{/* <h1>{quotes.text}</h1> */}
 			{(auth) ? <Salutation name="Godlive" /> : <Welcome />}
 
 			<h5>Il est {dateToday.toLocaleDateString() + ' a ' + dateToday.toLocaleTimeString()}</h5>
