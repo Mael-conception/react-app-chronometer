275158132df5ad50ea05d5988c9bef9ec60e54b0
vanotis720 <vanotis720@gmail.com>
2023-05-08 18:09:34 +0200

    state and effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Salutation from './Components/Salutation';
 import Welcome from './Components/Welcome';
 
 function App() {
+
+	const [dateToday, setDateToday] = useState(new Date());
+	const [auth, setAuth] = useState(false);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDateToday(new Date());
+			setAuth(true);
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+			console.log('unmouted');
+		}
+	});
+
 	return (
 		<div className="App">
 			<h1 id="">Hello</h1>
-			<Salutation name="Godlive" />
-			<Welcome name="Marie" />
+
+			{(auth) ? <Salutation name="Godlive" /> : <Welcome />}
+
+			<h5>Il est {dateToday.toLocaleDateString() + ' a ' + dateToday.toLocaleTimeString()}</h5>
 		</div>
 	);
 }
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,8 +2,28 @@ import React from "react";
 
 class Welcome extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'name': 'Mario Bros'
+        }
+    }
+
+    componentDidMount() {
+        console.log('================welcome mounted====================');
+        console.log('mounted');
+        console.log('====================================');
+    }
+
+    componentWillUnmount() {
+        alert('security  risk');
+    }
+
     render() {
-        return <p>Bienvenue {this.props.name}</p>
+        return (
+            <p>Bienvenue { (this.props.name) ? <b>{this.props.name}</b> : <span>{this.state.name}</span> }</p>
+        );
     }
 }
 
